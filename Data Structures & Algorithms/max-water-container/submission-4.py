aa0227a3cbb32e0a8f8fee3dc3b479fbca4de25f
Abdu5524-cpu <auralov@wesleyan.edu>
2026-07-21 10:26:26 -0400

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l = 0
+        r = len(heights)-1
+        stone = True
+        memo = set()
+        
+        while l < r:
+            memo.add(min(heights[l], heights[r]) * (r-l))
+
+            if heights[l] < heights[r]:
+                while heights[l] == heights[l+1] and l < r:
+                    l += 1
+                l += 1
+            elif heights[l] > heights[r]:
+                while heights[r] == heights[r-1] and l < r:
+                    r -= 1
+                r -= 1
+            elif heights[l] == heights[r]:
+                if heights[l+1] > heights[r-1]:
+                    l += 1
+                elif heights[l+1] < heights[r-1]:
+                    r -= 1
+                elif heights[l+1] == heights[r-1]:
+                    l += 1
+                    r -= 1
+
+
+
+
+
+
+        return max(memo)
